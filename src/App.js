d22aaa74311a45856afc633b23ad397c6b12d1f7
gar4a <gar4a@hotmail.fr>
2020-02-13 18:29:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Pages/Login';
+import './index.css'
+
+const App = () => {
+  return (
+      <div>
+         <Login/>
+      </div>
+  )
+}
+export default App;
---
+++ src/Pages/Login.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from '../images/bg-login.jpg'
+
+class Login extends React.Component {
+    render() {
+      return (
+        <body className="body">
+          <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-43">
+						Login to continue
+					</span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email"/>
+						<span class="focus-input100"></span>
+						<span class="label-input100">ID</span>
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="pass"/>
+						<span class="focus-input100"></span>
+						<span class="label-input100">Password</span>
+					</div>
+
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+
+						<div>
+							<a href="#" class="txt1">
+								Forgot Password?
+							</a>
+						</div>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+				</form>
+
+				<div class="login100-more logo">
+				<img className="responsive" src={logo} alt="Logo" />;
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+	
+
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="vendor/select2/select2.min.js"></script>
+
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+
+	<script src="js/main.js"></script>
+    </body>
+      )
+    }
+}
+
+export default Login;
