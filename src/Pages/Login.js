c800755d8ca4f21c1b5e0e3f5c358414e775ef6c
gar4a <gar4a@hotmail.fr>
2020-02-15 21:15:48 +0100

    changes

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from '../images/bg-login.png'
-import logo from '../images/bg-login.png'
 
 class Login extends React.Component {
     render() {
