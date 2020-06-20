8711799a190f1b42c6d80ced38376c02b2619b56
gar4a <gar4a@hotmail.fr>
2020-06-20 18:50:01 +0200

    .

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,16 +1,17 @@
-import { SELECT_PRODUCT } from "../constants/types";
+import { SELECT_PRODUCT, FETCH_PRODUCTS_SUCCESS } from "../constants/types";
 import data from "../Pages/data";
 
 const initialState = {
   selectedProduct: {},
-  productList: data
+  productList: [],
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case SELECT_PRODUCT:
       return { ...state, selectedProduct: payload };
-
+    case FETCH_PRODUCTS_SUCCESS:
+      return { ...state, productList: payload };
     default:
       return state;
   }
